8e20781beca49722a77c5c42334d17f672a7109b
Matthias Schuetz <mschuetz@gmail.com>
2013-03-19 09:30:48 +0100

    added cron monitoring handler, basic chat message handler and removed some test code

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 #!/usr/bin/env python
 
 import webapp2
+import re
+import logging
 from google.appengine.api import xmpp
 from google.appengine.ext import db
 from google.appengine.api import users
+from google.appengine.api.urlfetch import fetch
 
+USERS = ('mschuetz@gmail.com',)
 
 class NotificationReceiver(db.Model):
   name = db.StringProperty(required=True)
   jid = db.StringProperty(required=True)
 
 class InviteHandler(webapp2.RequestHandler):
-    def get(self):
+    def post(self):
         user = users.get_current_user()
         if user:
             self.response.write("Welcome, %s! I'll send you an XMPP invite" % (user.nickname(),))
@@ -21,29 +25,54 @@ class InviteHandler(webapp2.RequestHandler):
         else:
             self.response.write("<a href=\"%s\">Sign in or register</a>." % users.create_login_url("/"))
 
+def notify_all(msg, *args):
+    for user in USERS:
+        xmpp.send_message(user, msg % args)
+
 class MonitorHandler(webapp2.RequestHandler):
-    def get(self, site):
-        self.response.write('monitoring .. ' + site)
+    def get(self):
+        for site in db.GqlQuery("select url from MonitorSubject"):
+            try: 
+                res = fetch(site.url)
+                if res.status_code != 200:
+                    notify_all('fetch(%s):\nstatus %s', site.url, res.status_code)
+            except e:
+                notify_all('fetch(%s):\nraised exception %s', site.url, e)
 
-class ReceiversHandler(webapp2.RequestHandler):
-    def get(self):
-        for receiver in db.GqlQuery("SELECT jid FROM NotificationReceiver"):
-            self.response.write('receiver: ' + receiver.jid)
- 
-class XMPPHandler(webapp2.RequestHandler):
-    def get(self):
-        res = xmpp.send_message('mschuetz@gmail.com', 'hello there')
-        msg = 'no match'
-        if res == xmpp.NO_ERROR:
-            msg = 'no error'
-        elif res == xmpp.INVALID_JID:
-            msg = 'invalid jid'
-        elif res == xmpp.OTHER_ERROR:
-            msg = "other_error"
+class MonitorSubject(db.Model):
+    url = db.StringProperty(required=True)
+
+def add_url(url):
+    MonitorSubject(url=url).put()
+    'ok'
+
+def remove_url(url):
+    db.GqlQuery('delete from MonitorSubject where url=%s' % url)
+    'ok'
+
+def list_urls():
+    string.join([site.url for site in db.GqlQuery('select from MonitorSubject')], '\n')
+
+MSG_HANDLERS = {'^add (.*)$', add_url, '^rm (.*)$', remove_url, '^list', list_urls, '^ping', lambda : 'pong'}
+
+class MessageHandler(webapp2.RequestHandler):
+    def post(self):
+        message = xmpp.Message(self.request.POST)
+        logging.info("message from %s", message.sender)
+        jid_without_resource = re.match('^(.*@.*)/.*$', message.sender).group(1)
+        if jid_without_resource not in USERS:
+            message.reply('you\'re not authorized to use this service')
+            raise Exception('message from unauthorized sender')
+        msg = message.body.lower()
         
-        self.response.write('sent message: ' + msg)
+        if msg == 'ping':
+            message.reply('pong')
+        for regex, handler in MSG_HANDLERS:
+            match = re.match(regex, msg)
+            if match:
+                message.reply(handler(*match.groups()))
+            
 
-app = webapp2.WSGIApplication([('/asdf', XMPPHandler), ('/invite', InviteHandler),
-                               webapp2.Route('/monitor/<site>', MonitorHandler),
-                               ('/receivers', ReceiversHandler)],
+app = webapp2.WSGIApplication([('/_ah/xmpp/message/chat/', MessageHandler),
+                               ('/monitor', MonitorHandler), ('/invite', InviteHandler)],
                               debug=True)
